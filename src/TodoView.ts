148d32d239950a92aaf916bad6982f0b03cd6235
Amelithic <amelithic@gmail.com>
2026-01-18 13:23:37 +0000

    TodoView scanForTodo() - working 1

--- src/TodoView.ts
+++ src/TodoView.ts
@@ -1,8 +1,14 @@
 /* eslint-disable obsidianmd/ui/sentence-case */
 import TodoPlugin from "main";
-import { ItemView, WorkspaceLeaf} from "obsidian";
+import { ItemView, TFile, WorkspaceLeaf} from "obsidian";
 
 export const TODO_VIEW_TYPE = "todo-view";
+
+export interface TodoItem {
+    text: string;
+    file: TFile;
+    line: number;
+}
 
 export class TodoView extends ItemView {
     plugin: TodoPlugin;
@@ -30,10 +36,54 @@ export class TodoView extends ItemView {
         container?.empty();
         container?.createEl("h2", { text: "My Todo View" });
         container?.createEl("p", { text: currentTodoMarker});
+
+        let todos = await this.scanForTodo();
+        const todoDiv = container?.createDiv();
+        if (todos) {
+            for (let todo of todos) {
+                todoDiv?.createEl("p", { text : todo.text });
+            }
+        }
     
     }
 
     async onClose() {
         // cleanup if needed
+    }
+
+    async scanForTodo() {
+		//get settings marker value
+		const markers = this.plugin.settings.todoMarker
+            .split(',')
+            .map(m => m.trim()) //remove trailing spaces for each
+            .filter(m => m.length > 0);
+		const files = this.app.vault.getMarkdownFiles();
+
+		let todos: TodoItem[] = []; //blank TodoItem array
+
+		for (const file of files) {
+			const content = await this.app.vault.read(file); //files content
+            const lines = content.split("\n");
+
+            //scan each line
+            lines.forEach((line, index) => {
+                for (let marker of markers) {
+                    if (line.includes(marker)) {
+                        todos.push({
+                            text: line.trim(),
+                            file,
+                            line: index + 1, // human-readable line numbers
+                        });
+                    }
+                }
+                
+            });
+	    }
+
+        console.log("Markers:", markers);
+        console.log("Files scanned:", files.length);
+        console.log("Todos found:", todos);
+
+        return todos;
     }
 }
